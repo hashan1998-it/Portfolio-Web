7631066ad45527a565f6ce9969c495f9fbe097d5
Hashan Danushka <hashansooriyage@gmail.com>
2023-10-23 23:57:16 +0530

    Added Typing Text Animation

--- src/Components/HomeBody.jsx
+++ src/Components/HomeBody.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Typewriter } from "react-simple-typewriter";
 
 function HomeBody() {
   return (
@@ -8,7 +9,18 @@ function HomeBody() {
       <div className="flex justify-center items w-1/2">
         {/* left side */}
         <div className="flex flex-col justify-center w-1/2">
-          <h1 className="text-4xl font-bold">Mobile / Web developer👋</h1>
+          <h1 className="text-4xl font-bold">
+            <Typewriter
+              words={["Hi,I'm Hashan", "Mobile Developer", "Web Developer"]}
+              loop={100}
+              cursor
+              cursorStyle="_"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+            👋
+          </h1>
           <p className="font-thin pt-5">
             Hi, I'm Hashan. I'm a passionate mobile and web developer and I
             create digital magic with a passion for design and development.💻
@@ -25,7 +37,10 @@ function HomeBody() {
 
         {/* right side */}
         <div className="flex w-1/2 justify-end">
-          <img src="../../assets/images/my.jpg" className="h-40 home-img shape" />
+          <img
+            src="../../assets/images/my.jpg"
+            className="h-40 home-img shape"
+          />
         </div>
       </div>
       <div className="flex pt-16 w-1/2">
@@ -33,13 +48,12 @@ function HomeBody() {
         <div className="vl h-8 ml-5 border-l-4 border-blue-400"></div>
         <div className="flex">
           <img className="pl-10 h-8" src="../../assets/images/html.png" />
-          <img className="px-5 h-8" src="../../assets/images/css.png"  />
-          <img className="px-5 h-8" src="../../assets/images/js.png"  />
-          <img className="px-5 h-8" src="../../assets/images/laravel.png"  />
-          <img className="px-5 h-8" src="../../assets/images/react.png"  />
-          <img className="px-5 h-8" src="../../assets/images/flutter.png"  />
-          <img className="px-5 h-8" src="../../assets/images/tailwind.png"  />
-          
+          <img className="px-5 h-8" src="../../assets/images/css.png" />
+          <img className="px-5 h-8" src="../../assets/images/js.png" />
+          <img className="px-5 h-8" src="../../assets/images/laravel.png" />
+          <img className="px-5 h-8" src="../../assets/images/react.png" />
+          <img className="px-5 h-8" src="../../assets/images/flutter.png" />
+          <img className="px-5 h-8" src="../../assets/images/tailwind.png" />
         </div>
       </div>
     </div>
